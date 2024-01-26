69cb49ca71f55c0f469016ca910b7c43b31ae337
Philip Brown <pwbrown24@gmail.com>
2024-01-26 10:07:50 -0600

    first pass at state management

--- lego-mustang/src/main.cpp
+++ lego-mustang/src/main.cpp
@@ -1,63 +1,278 @@
 #include "cJSON.h"
-#include "pins.h"
-#include <Enums.h>
+#include "settings.h" // Includes pin, topic, and behavior settings
 #include <Light.h>
 #include <MqttClient.h>
 #include <SequentialLightGroup.h>
 #include <Utils.h>
+#include <cJSON.h>
+#include <string>
 
 // Export main function for C compiler
 extern "C" {
 void app_main(void);
 }
 
-// ********************* STATES *********************
-Enums::LightMode lightMode = Enums::LightMode::Off;
+// ********************* STATE DEFINITIONS *********************
+
+// Lighting Modes
+#define LIGHT_MODE_OFF "OFF"           // Lights Off
+#define LIGHT_MODE_RUNNING "RUNNING"   // Daytime Running Lights
+#define LIGHT_MODE_LOW_BEAM "LOW_BEAM" // Low Beams
+
+// Light Switch Modes
+#define SWITCH_ON "ON"   // Light Channel On
+#define SWITCH_OFF "OFF" // Light Channel Off
+
+// Turning Modes
+#define TURNING_OFF "OFF"     // No turn signal
+#define TURNING_LEFT "LEFT"   // Left turn signal
+#define TURNING_RIGHT "RIGHT" // Right turn signal
+
+// ********************* STATES ***********************
+std::string lightingState = LIGHT_MODE_OFF;
+std::string highBeamState = SWITCH_OFF;
+std::string turningState = TURNING_OFF;
+std::string reverseState = SWITCH_OFF;
+std::string fogState = SWITCH_OFF;
+std::string interiorState = SWITCH_OFF;
+std::string hazardState = SWITCH_OFF;
 
 // ********************* MQTT CLIENT SETUP *********************
-MqttClient client; // MQTT Client
+MqttClient client("lego_mustang"); // MQTT Client
 
-void setupMqttClient() {}
-
-// PWM Channel incremental (increment every time it is used)
-int pwmChannel = 0;
+// ********************* LIGHT SETUP *************************
+int pwmChannel = 0; // incremental PWM channel
 
 // Left Headlight
-Light leftHeadlight(LEFT_HEADLIGHT, pwmChannel++);
+Light leftHeadlight(LEFT_HEADLIGHT_PIN, pwmChannel++);
 
 // Right Headlight
-Light rightHeadlight(RIGHT_HEADLIGHT, pwmChannel++);
+Light rightHeadlight(RIGHT_HEADLIGHT_PIN, pwmChannel++);
 
-// Left Taillight
-Light leftInnerTaillight(LEFT_INNER_TAILLIGHT, pwmChannel++);
-Light leftMiddleTaillight(LEFT_MIDDLE_TAILLIGHT, pwmChannel++);
-Light leftOuterTaillight(LEFT_OUTER_TAILLIGHT, pwmChannel++);
+// Left Taillight Group
+Light leftInnerTaillight(LEFT_INNER_TAILLIGHT_PIN, pwmChannel++);
+Light leftMiddleTaillight(LEFT_MIDDLE_TAILLIGHT_PIN, pwmChannel++);
+Light leftOuterTaillight(LEFT_OUTER_TAILLIGHT_PIN, pwmChannel++);
 SequentialLightGroup leftTaillight(leftInnerTaillight, leftMiddleTaillight,
                                    leftOuterTaillight);
 
-// Right Taillight
-Light rightInnerTaillight(RIGHT_INNER_TAILLIGHT, pwmChannel++);
-Light rightMiddleTaillight(RIGHT_MIDDLE_TAILLIGHT, pwmChannel++);
-Light rightOuterTaillight(RIGHT_OUTER_TAILLIGHT, pwmChannel++);
+// Right Taillight Group
+Light rightInnerTaillight(RIGHT_INNER_TAILLIGHT_PIN, pwmChannel++);
+Light rightMiddleTaillight(RIGHT_MIDDLE_TAILLIGHT_PIN, pwmChannel++);
+Light rightOuterTaillight(RIGHT_OUTER_TAILLIGHT_PIN, pwmChannel++);
 SequentialLightGroup rightTaillight(rightInnerTaillight, rightMiddleTaillight,
                                     rightOuterTaillight);
 
 // Other lights
-Light fogLights(FOG_LIGHTS);
-Light runningLights(RUNNING_LIGHTS);
-Light reverseLights(REVERSE_LIGHTS);
-Light interiorLights(INTERIOR_LIGHTS);
+Light fogLights(FOG_LIGHTS_PIN);
+Light runningLights(RUNNING_LIGHTS_PIN);
+Light reverseLights(REVERSE_LIGHTS_PIN);
+Light interiorLights(INTERIOR_LIGHTS_PIN);
 
-// Turn off all lights
-void allOff(void) {
-  runningLights.off();
-  fogLights.off();
-  leftHeadlight.off();
-  rightHeadlight.off();
-  interiorLights.off();
-  leftTaillight.off();
-  rightTaillight.off();
-  reverseLights.off();
+// ************************ STATE UPDATES **********************
+
+/** Update all lights based on the current state */
+void updateLightsFromState(void) {
+  // Fog lights
+  fogState == SWITCH_ON ? fogLights.on() : fogLights.off();
+  // Reverse lights
+  reverseState == SWITCH_ON ? reverseLights.on() : reverseLights.off();
+  // Interior lights
+  interiorState == SWITCH_ON ? interiorLights.on() : interiorLights.off();
+  // Lights switched off
+  if (lightingState == LIGHT_MODE_OFF) {
+    runningLights.off();
+    leftHeadlight.off();
+    rightHeadlight.off();
+    leftTaillight.off();
+    rightTaillight.off();
+  }
+  // Daytime running lights
+  else if (lightingState == LIGHT_MODE_RUNNING) {
+    runningLights.on();
+    leftHeadlight.off();
+    rightHeadlight.off();
+    leftTaillight.on(RUNNING_BRIGHTNESS);
+    rightTaillight.on(RUNNING_BRIGHTNESS);
+  }
+  // Low beams
+  else if (lightingState == LIGHT_MODE_LOW_BEAM) {
+    runningLights.on();
+    leftHeadlight.on(LOW_BEAM_BRIGHTNESS);
+    rightHeadlight.on(LOW_BEAM_BRIGHTNESS);
+    leftTaillight.on(LOW_BEAM_BRIGHTNESS);
+    rightTaillight.on(LOW_BEAM_BRIGHTNESS);
+  }
+  // Hazards
+  if (hazardState == SWITCH_ON) {
+    leftHeadlight.blink(BLINKING_INTERVAL);
+    rightHeadlight.blink(BLINKING_INTERVAL);
+    leftTaillight.blink(BLINKING_INTERVAL);
+    rightTaillight.blink(BLINKING_INTERVAL);
+  }
+  // Turning left
+  else if (turningState == TURNING_LEFT) {
+    leftHeadlight.blink(BLINKING_INTERVAL);
+    leftTaillight.start(BLINKING_INTERVAL, SEQUENTIAL_INTERVAL);
+  }
+  // Turning right
+  else if (turningState == TURNING_RIGHT) {
+    rightHeadlight.blink(BLINKING_INTERVAL);
+    rightTaillight.start(BLINKING_INTERVAL, SEQUENTIAL_INTERVAL);
+  }
+}
+
+/**
+ * Constructs a JSON representation of the current state and publishes it to the
+ * MQTT client
+ */
+void publishCurrentState(void) {
+  // Create state object and fill with state values
+  cJSON *state = cJSON_CreateObject();
+  cJSON_AddStringToObject(state, "lighting", lightingState.c_str());
+  cJSON_AddStringToObject(state, "highBeam", highBeamState.c_str());
+  cJSON_AddStringToObject(state, "turning", turningState.c_str());
+  cJSON_AddStringToObject(state, "reverse", reverseState.c_str());
+  cJSON_AddStringToObject(state, "fog", fogState.c_str());
+  cJSON_AddStringToObject(state, "interior", interiorState.c_str());
+  cJSON_AddStringToObject(state, "hazard", hazardState.c_str());
+  // Convert state object to string
+  char *stateStr = cJSON_PrintUnformatted(state);
+  // Deallocate state object (MUST be done to avoid memory leaks)
+  cJSON_Delete(state);
+  // Publish state to topic
+  client.publish(PUB_STATE_TOPIC, stateStr, true);
+}
+
+// *********************** SUBSCRIPTION UTILITIES ***********************
+
+// Checks if the payload data is a valid light mode string
+bool isLightModeStr(std::string data) {
+  return (data == LIGHT_MODE_OFF || data == LIGHT_MODE_RUNNING ||
+          data == LIGHT_MODE_LOW_BEAM);
+}
+
+// Checks if the payload data is a valid turning string
+bool isTurningStr(std::string data) {
+  return data == TURNING_OFF || data == TURNING_LEFT || data == TURNING_RIGHT;
+}
+
+// Checks if the payload data is a valid switch string
+bool isSwitchStr(std::string data) {
+  return data == SWITCH_ON || data == SWITCH_OFF;
+}
+
+/**
+ * Handles validating data and applying state updates for a topic subscription
+ * @param data The data string payload from the topic subscription
+ * @param state Reference to a state variable that should be updated
+ * @param validate A pointer to a validation function to check the data
+ */
+void handleSubscription(std::string data, std::string &state,
+                        bool (*validate)(std::string)) {
+  if (!validate(data)) {
+    return;
+  }
+  state = data;
+  updateLightsFromState();
+  publishCurrentState();
+}
+
+// Alias for handleSubscripton with the isSwitchStr validation function
+void handleSwitchSubscription(std::string data, std::string &state) {
+  handleSubscription(data, state, &isSwitchStr);
+}
+
+// *********************** SUBSCRIPTION CALLBACKS *********************
+
+/**
+ * Set the state of all lights simultaneously
+ * @param data Should be ON or OFF
+ */
+void setAllLights(std::string data) {
+  if (!isSwitchStr(data)) {
+    return;
+  }
+  // Turn off all lights
+  if (data == SWITCH_OFF) {
+    lightingState = LIGHT_MODE_OFF;
+    highBeamState = SWITCH_OFF;
+    turningState = TURNING_OFF;
+    reverseState = SWITCH_OFF;
+    fogState = SWITCH_OFF;
+    interiorState = SWITCH_OFF;
+    hazardState = SWITCH_OFF;
+  }
+  // Turn all lights on with no effects
+  else if (data == SWITCH_ON) {
+    lightingState = LIGHT_MODE_LOW_BEAM;
+    highBeamState = SWITCH_OFF;
+    turningState = TURNING_OFF;
+    reverseState = SWITCH_ON;
+    fogState = SWITCH_ON;
+    interiorState = SWITCH_ON;
+    hazardState = SWITCH_OFF;
+  }
+  updateLightsFromState();
+  publishCurrentState();
+}
+
+/**
+ * Sets the lighting mode. This only affects running lights, headlights, and
+ * taillights
+ * @param data Should be OFF, RUNNING, or LOW_BEAM
+ */
+void setLightState(std::string data) {
+  handleSubscription(data, lightingState, &isLightModeStr);
+}
+
+/**
+ * Sets the high beam state. This overrides the behavior of the low beam
+ * lighting mode while on
+ * @param data Should be ON or OFF
+ */
+void setHighBeamState(std::string data) {
+  handleSwitchSubscription(data, highBeamState);
+}
+
+/**
+ * Sets the turning state. This overrides the behavior of the low beam
+ * lighting mode while on
+ * @param data Should be OFF, LEFT, or RIGHT
+ */
+void setTurningState(std::string data) {
+  handleSubscription(data, turningState, &isTurningStr);
+}
+
+/**
+ * Sets the reverse lights state. This is a standalone effect
+ * @param data Should be ON or OFF
+ */
+void setReverseState(std::string data) {
+  handleSwitchSubscription(data, reverseState);
+}
+
+/**
+ * Sets the fog lights state. This is a standalone effect
+ * @param data Should be ON or OFF
+ */
+void setFogState(std::string data) { handleSwitchSubscription(data, fogState); }
+
+/**
+ * Sets the interior lights state. This is a standalone effect
+ * @param data Should be ON or OFF
+ */
+void setInteriorState(std::string data) {
+  handleSwitchSubscription(data, interiorState);
+}
+
+/**
+ * Sets the hazard lights state. This effect overrides low beam, high beam, and
+ * turning states
+ * @param data Should be ON or OFF
+ */
+void setHazardState(std::string data) {
+  handleSwitchSubscription(data, hazardState);
 }
 
 /**
@@ -77,8 +292,7 @@ void loop(unsigned int now) {
 void app_main(void) {
   Light::configurePWMTimer();
 
-  // Turn off all lights at the beginning
-  allOff();
+  updateLightsFromState();
 
   // Start main loop
   Utils::startLoop(&loop);
---
+++ lego-mustang/src/settings.h
@@ -0,0 +1,37 @@
+
+/***************** GPIO PINS ******************/
+
+#define LEFT_HEADLIGHT_PIN 16
+#define RIGHT_HEADLIGHT_PIN 13
+#define FOG_LIGHTS_PIN 18
+#define RUNNING_LIGHTS_PIN 17
+#define INTERIOR_LIGHTS_PIN 19
+#define LEFT_INNER_TAILLIGHT_PIN 23
+#define LEFT_MIDDLE_TAILLIGHT_PIN 22
+#define LEFT_OUTER_TAILLIGHT_PIN 21
+#define RIGHT_INNER_TAILLIGHT_PIN 33
+#define RIGHT_MIDDLE_TAILLIGHT_PIN 25
+#define RIGHT_OUTER_TAILLIGHT_PIN 26
+#define REVERSE_LIGHTS_PIN 32
+
+/************** LIGHTING BEHAVIOR ************/
+
+#define RUNNING_BRIGHTNESS 25    // How bright should lights be in running mode
+#define LOW_BEAM_BRIGHTNESS 50   // How bright are the low beams when on
+#define HIGH_BEAM_BRIGHTNESS 100 // How bright are the high beams when on
+#define BLINKING_INTERVAL 500    // What is the blinking interval in ms
+#define SEQUENTIAL_INTERVAL 100  // What is the sequential effect delay in ms
+
+/***************** MQTT TOPICS ****************/
+
+#define BASE_TOPIC "/lego/mustang/" // The base topic path for all other topics
+
+#define PUB_STATE_TOPIC BASE_TOPIC "state"       // For reporting current state
+#define SUB_LIGHTING_TOPIC BASE_TOPIC "lighting" // Update the lighting mode
+#define SUB_HIGH_BEAM_TOPIC BASE_TOPIC "high_beam" // Update the high beams
+#define SUB_TURNING_TOPIC BASE_TOPIC "turning"     // Update the turning mode
+#define SUB_REVERSE_TOPIC BASE_TOPIC "reverse"     // Update the reverse lights
+#define SUB_FOG_TOPIC BASE_TOPIC "fog"             // Update the fog lights
+#define SUB_INTERIOR_TOPIC BASE_TOPIC "interior"   // Update the interior lights
+#define SUB_HAZARD_TOPIC BASE_TOPIC "hazard"       // Update the hazard lights
+#define SUB_ALL_TOPIC BASE_TOPIC "all"             // All lights on/off
